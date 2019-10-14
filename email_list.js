00e340a7b3338aa8c8f6f668d9b2a4262397f151
snehashakya <54903441+snehashakya@users.noreply.github.com>
2019-10-13 22:43:43 -0500

    Add files via upload

---
+++ email_list.js
@@ -0,0 +1,39 @@
+const $ = function (id) {
+    return document.getElementById(id);
+}
+
+const joinList = function () {
+    const emailAddress1 = $('email_address1').value;
+    const emailAddress2 = $('email_address2').value;
+    let isValid = true;
+
+    if (emailAddress1 === '') {
+        $('email_address1_error').firstChild.nodeValue = 'This field is required.';
+        isValid = false;
+    } else {
+        $('email_address1_error').firstChild.nodeValue = '';
+    }
+
+    if (emailAddress1 !== emailAddress2) {
+        $('email_address2_error').firstChild.nodeValue = 'This entry must equal first entry.';
+        isValid = false;
+    } else {
+        $('email_address2_error').firstChild.nodeValue = '';
+    }
+
+    if ($('first_name').value === '') {
+        $('first_name_error').firstChild.nodeValue = 'This field is required.';
+        isValid = false;
+    } else {
+        $('first_name_error').firstChild.nodeValue = '';
+    }
+
+    if (isValid) {
+        $('email_form').submit();
+    }
+}
+
+window.onload = function () {
+    $('join_list').onclick = joinList;
+    $('email_address1').focus();
+}
