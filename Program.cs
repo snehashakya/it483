ca25db7e74c7cdfe9e4b7471c032144b663ab85f
snehashakya <54903441+snehashakya@users.noreply.github.com>
2019-12-02 21:00:18 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,68 +1,49 @@
 ﻿using System;
 
-namespace Lab04
+namespace LAB03_Group
 {
 	class Program
 	{
-		public static double average(int n1, int n2)
+		public static double transaction(double Bal, double amount)
 		{
-			return (n1 + n2) / 2.0;
+			double RecentAmount = 0.0;
+
+			if ((amount < 0 && Bal > amount) || amount > 0)
+				RecentAmount = amount;
+
+			return RecentAmount;
 		}
 
-		static double average(int a, int b, int c)
+		public static void Main(string[] args)
 		{
-			return (a + b + c) / 3.0;
+			double curBal = 45.32;
+
+			Console.Write("Please enter a amount to update account by $");
+
+			double amount = Int32.Parse(Console.ReadLine());
+
+			Console.Write("\n");
+
+			Console.Write("Customer\'s balance (before transaction) = $");
+
+			Console.WriteLine(curBal + "\n");
+
+			Console.Write("Requested update amount = $");
+
+			Console.WriteLine(amount + "\n");
+
+
+			double actAmount = transaction(curBal, amount);
+
+			curBal += actAmount;
+
+			Console.Write("Actual update amount = $");
+			Console.WriteLine(actAmount + "\n");
+			Console.Write("Customer\'s balance (after transaction) = $");
+			Console.WriteLine(curBal + "\n");
+
+			Console.WriteLine("\nThank you and good-bye!\n");
 		}
-
-		static double average(double a, double b)
-		{
-			return (a + b) / 2.0;
-		}
-
-		static double average(double a, double b, double c)
-		{
-			return (a + b + c) / 3.0;
-		}
-
-		public static void Main(String[] args)
-		{
-
-			int a = 1, b = 3, c = 5;
-
-			double x = 2.2, y = 4.4, z = 6.6, ans;
-
-			ans = average(a, b);
-
-			Console.WriteLine("\naverage(a, b) = " + ans);
-
-			ans = average(a, b, c);
-
-			Console.WriteLine("\naverge(a, b, c) = " + ans);
-
-			ans = average(x, y);
-
-			Console.WriteLine("\naverage(x, y) = " + ans);
-
-			ans = average(x, y, z);
-
-			Console.WriteLine("\naverage(x, y, z) = " + ans);
-		}
-
 	}
+
 }
-
-
-//Q/A
-//1.Do you really need the int parameter version(s)ofaverage,and why?
-//Int parameter version helps us to give reference to the name space on the code so yes we need it.
-//Without the defination we cannot able to run the code
-
-//2.Do you really need the three parameter version of average,i.e.is average(average(a, b),c)== average(a, b, c),and why?
-//We need the three parameter for the average of a,b,c because 
-//without the three different numbers we are unable to calculate the average mean of numbers on the coding 
-//as the average three diffferent numbers will have different value than the average of two numbers and a third number.
-//There is difference in the average(average(a, b), c) and average(a, b, c).
-
-//3.Is average(1,2.0,3)legal,and if so which version is used,and why?
-//The average(1,2.0,3) if definately legal.
-//The average(double a, double b, double c) version is used because  in a double argument we can only use this method.
